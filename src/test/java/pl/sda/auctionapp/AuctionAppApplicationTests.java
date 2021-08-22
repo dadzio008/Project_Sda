d70404835b6e76d2a2100fd7f240599c7b49debb
Krzysztof Chęciński <checinski.krzysztof@gmail.com>
2021-08-22 10:14:15 +0200

    first settings

---
+++ src/test/java/pl/sda/auctionapp/AuctionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.auctionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
